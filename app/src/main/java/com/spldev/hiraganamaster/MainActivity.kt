7f384b26bd54124ae3bfe6d820674195eec78f5f
Suar <fn7sephiroth@gmail.com>
2025-03-21 21:37:09 +0100

    optimize imports

--- app/src/main/java/com/spldev/hiraganamaster/MainActivity.kt
+++ app/src/main/java/com/spldev/hiraganamaster/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.spldev.hiraganamaster
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.google.mlkit.vision.digitalink.Ink
 import com.spldev.hiraganamaster.ui.navigation.AppNavigation
-import com.spldev.hiraganamaster.ui.screens.HiraganaPracticeScreen
-import com.spldev.hiraganamaster.ui.screens.LoginScreen
-import com.spldev.hiraganamaster.ui.screens.RegisterScreen
-import com.spldev.hiraganamaster.ui.screens.SelectionScreen
 import com.spldev.hiraganamaster.ui.viewmodel.HiraganaViewModel
 import com.spldev.hiraganamaster.ui.viewmodel.LoginViewModel
 import com.spldev.hiraganamaster.ui.viewmodel.RegisterViewModel
